b02edd6a9c975a1b05ef995c63e5295f60c7a559
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-05-17 11:08:00 +0200

    create app structure and constants

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "646496d3e29d96ad4ed9";
+  static const String projectId = "64648fdf108f213a1745";
+  static const String endPoint = "https://appwrite.io/v1";
+}
